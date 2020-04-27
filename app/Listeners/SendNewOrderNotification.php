bbc0f3605e0596f589690055f565ca157fae6ce5
Tobi <tohbyyy@gmail.com>
2020-04-27 14:24:14 +0700

    OrderReceived

--- app/Listeners/SendNewOrderNotification.php
+++ app/Listeners/SendNewOrderNotification.php
@@ -7,7 +7,7 @@ use App\Events\OrderPlaced;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Notification;
-use App\Notifications\OrderSent;
+use App\Notifications\OrderReceived;
 
 class SendNewOrderNotification
 {
